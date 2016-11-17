6af0a12064ab2cf5187558bfe9f40d49955ac847
Ananasy <anastasiya_johnni@mail.ru>
2016-11-17 15:52:36 +0300

    teacher auth support

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,32 @@
 module.exports = function(app, db){
     app.get('/auth/api/:id', function(req,res){
         var unikey = req.params.id;
-        if (req.query.type === 'student') {
-            db.query('SELECT * FROM students WHERE unikey = ?', unikey, function(error, results, fields){
-                if (error){
-                    res.sendStatus(500);
-                }
-                if (results.length > 0){
-                    res.send(results[0]);
-                }
-            })
+        var password = decodeURIComponent(req.query.hash).replace(" ","+");
+        switch (req.query.type) {
+            case 'student': {
+                db.query('SELECT * FROM students WHERE unikey = ?', unikey, function (error, results, fields) {
+                    if (error) {
+                        res.sendStatus(500);
+                    }
+                    if (results.length > 0) {
+                        res.send(results[0]);
+                    }
+                });
+                break;
+            }
+            case 'teacher': {
+                db.query('SELECT * FROM professors WHERE unikey = ? AND password = ?', [unikey, password],
+                    function (error, results, fields) {
+                    if (error) {
+                        res.sendStatus(500);
+                    }
+                    if (results.length > 0) {
+                        res.send(results[0]);
+                    }
+                });
+                break;
+            }
+
         }
     })
 };
