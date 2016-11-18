0648c2bd035c21a3e2df42e24cc2228221bb8f78
Ananasy <anastasiya_johnni@mail.ru>
2016-11-18 23:00:15 +0300

    up

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 63333);
@@ -24,19 +26,31 @@ connection.connect(function(err) {
     require('./dbFillTestQuery/createStudentsSubjectsTable')(connection);
     require('./dbFillTestQuery/createClassroomsTable')(connection);
     require('./dbFillTestQuery/createProfessorsTable')(connection);
+    require('./dbFillTestQuery/createProfessorsSubjectsTable')(connection);
+    require('./dbFillTestQuery/createStudentsSubjectsTable')(connection);
     require('./dbFillTestQuery/addColumnToStudentsTable')(connection);
     require('./dbFillTestQuery/addNameColumnToStudentsTable')(connection);
     require('./dbFillTestQuery/fillStudentsTable')(connection);
     require('./dbFillTestQuery/fillSubjectsTable')(connection);
     require('./dbFillTestQuery/fillSubjectStudentTable')(connection);
-    require('./dbFillTestQuery/fillSubjectStudentTable')(connection);
+    require('./dbFillTestQuery/fillProfessorSubjectTable')(connection);
     require('./dbFillTestQuery/fillClassroomsTable')(connection);
+    require('./dbFillTestQuery/fillSubjectStudentTable')(connection);
+
  */
 
-require('./dbFillTestQuery/fillProfessorsTable')(connection);
+//require('./dbFillTestQuery/createMarksTable')(connection);
+//require('./dbFillTestQuery/createCheckRoomTrigger')(connection);
+//require('./dbFillTestQuery/createStudentsSubjectsTable')(connection);
+
+//require('./dbFillTestQuery/fillSubjectStudentTable')(connection);
+//require('./dbFillTestQuery/createCheckRoomTriggerOnUpdate')(connection);
+
+require('./routes/subjectAPI')(app,connection);
+require('./routes/teacherAPI')(app,connection, jsonParser);
 require('./routes/auth')(app,connection);
 require('./routes/studentAPI')(app,connection);
-
+require('./routes/roomsAPI')(app,connection);
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
