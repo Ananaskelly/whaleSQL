ab6cbbcedeba0733bd09d7965c68d03fad190adb
Ananaskelly <anastasiya_johnni@mail.ru>
2016-11-20 23:15:44 +0300

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-process.env['HOST'] = "localhost";
-process.env['DB_USER'] = "whale";
-process.env['DB_PASSWORD'] = "bazzingapank73";
-process.env['DB_DATABASE'] = "spbsu";
+process.env['HOST'] = "";
+process.env['DB_USER'] = "";
+process.env['DB_PASSWORD'] = "";
+process.env['DB_DATABASE'] = "";
